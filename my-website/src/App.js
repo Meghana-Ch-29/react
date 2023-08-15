63e32ad569eb48da41ee8f855d7dda4f6f04b0e4
Meghana Chebolu <meghanach29@gmail.com>
2023-08-15 16:38:26 +0000

    Added website intro

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -8,9 +8,15 @@ function App() {
       <Navbar /> {/* Place the Navbar component */}
       {/* Add your content here */}
       <div className="content">
-        <h1><span class="first-letter"><i>S</i></span>WASA</h1>
+        <h1><span className="first-letter"><i>S</i></span>WASA</h1>
+        <h2 className="welcome-text">Welcome to our website,</h2> {/* Add the class here */}
       </div>
-      {/* Your main content */}
+      <div className="welcome-para">
+        <i>A powerful tool designed to assist in
+              the early detection of respiratory diseases. Our platform employs
+              cutting-edge technology to analyze X-ray images and provide valuable insights into the probability of
+              several respiratory conditions, including pneumonia, tuberculosis, lung cancer, and COVID-19.</i>
+      </div>
     </div>
   );
 }
